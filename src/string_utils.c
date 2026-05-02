d3316cee8de045366787ccde9d043a9eb3713fd9
mou7med <wesd6026@gmail.com>
2026-05-02 23:20:54 +0100

    Update string_utils.c

--- src/string_utils.c
+++ src/string_utils.c
@@ -11,3 +11,27 @@ char* my_strcpy(char* dest, const char* src){
      // adding the character constant  
      return svd_dest ;
 }
+// -- modifing upper-to-lower case 
+void toLowerCase(char* s){
+     while ( *s != '\0') // to make sure *s has moved all along the char and stops by its end
+     {
+        if (*s > 'A' && *s < 'Z'){ // cheacking whiether *s is between A-Z
+            *s = *s + 32 ; // adding 32 to its ASC to make it lowercase 
+        }
+        s ++ ; // adding by one 
+     } 
+}
+// -- Palindrome
+int isPalindrome(const char* s){
+     int i = 0 , j = strlen(s) -1 ; // creating 2 intgers one at the begening and on by the end of the char
+     while (i < j) // to make sure we covered all the string without passing throgh all of it 
+     {
+        if(s[i] != s [j]){ // comparing 
+            return 0;// returning 0 in case they aint equal 
+        }
+        i++ ; 
+        j -- .
+     }
+      return 1 ; 
+}
+
